ea3d9d03558e854257c8965b07ce64b5a9d2bb98
sreyom31 <sreyom31@gmail.com>
2021-11-11 00:29:49 +0530

    Updating endpoints

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.post('/users', async (req, res) => {
 
 app.get('/users', async (req, res) => {
     try {
-        const users = await user.find({})
+        const users = await User.find({})
         res.send(users)
     } catch (e) {
         res.status(500).send()
@@ -43,24 +43,21 @@ app.get('/users/:id', async (req, res) => {
     }
 })
 
-app.get('/tasks', async (req, res) => {
-    try {
-        const tasks = Task.find({})
-        res.send(tasks)
-    } catch (e) {
-        res.status(500).send()
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid Updates!' })
     }
-})
-
-app.get('/tasks/:id', async (req, res) => {
-    const _id = req.params.id
 
     try {
-        const task = Task.findById( _id )
-        if(!task) {
-            return res.status(404).send('Task not found')
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        if(!user) {
+            return res.status(404).send('User not found')
         }
-        res.send(task)
+        res.send(user)
     } catch (e) {
         res.status(500).send(e)
     }
@@ -74,6 +71,49 @@ app.post('/tasks', async (req, res) => {
         res.status(201).send(task)
     } catch (e) {
         res.status(400).send(e)
+    }
+})
+
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.send(tasks)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+app.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const task = await Task.findById( _id )
+        if(!task) {
+            return res.status(404).send('Task not found')
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid Updates!' })
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        if(!task) {
+            return res.status(404).send('Task not found')
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(500).send(e)
     }
 })
 

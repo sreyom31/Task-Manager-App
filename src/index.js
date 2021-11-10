fa9ad4c2b2579e20155f1d33664109ccc36511ce
sreyom31 <sreyom31@gmail.com>
2021-11-11 00:35:13 +0530

    Deleting endpoints

--- src/index.js
+++ src/index.js
@@ -63,6 +63,18 @@ app.patch('/users/:id', async (req, res) => {
     }
 })
 
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+        if(!user) {
+            return res.status(404).send('User not found')
+        }
+        res.send(user)
+    } catch(e) {
+        res.status(500).send(e)
+    }
+})
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
@@ -113,6 +125,18 @@ app.patch('/tasks/:id', async (req, res) => {
         }
         res.send(task)
     } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if(!task) {
+            return res.status(404).send('User not found')
+        }
+        res.send(task)
+    } catch(e) {
         res.status(500).send(e)
     }
 })

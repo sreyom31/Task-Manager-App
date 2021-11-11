f695e7afe441eb94734a940cf0d35a9e938c699d
sreyom31 <sreyom31@gmail.com>
2021-11-11 21:56:19 +0530

    Generating authentication tokens

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
     name: {
@@ -40,9 +41,28 @@ const userSchema = mongoose.Schema({
             }
         }
     },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        },
+    }]
 
 })
 
+
+//* middleware for generating token
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'makingyourselfbetterwithtime')
+
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+
+    return token
+}
+
+//* middleware for login
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({ email })
 

3b096fca08c7d4f4bd6a5516c5cf497e73108be5
sreyom31 <sreyom31@gmail.com>
2021-11-12 01:34:21 +0530

    Authenticating task end points

--- src/routers/taskRoutes.js
+++ src/routers/taskRoutes.js
@@ -8,7 +8,7 @@ router.post('/tasks', authMiddleware, async (req, res) => {
         ...req.body,
         owner: req.user._id
     })
-    
+
     try {
         await task.save()
         res.status(201).send(task)
@@ -17,20 +17,23 @@ router.post('/tasks', authMiddleware, async (req, res) => {
     }
 })
 
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', authMiddleware, async (req, res) => {
     try {
-        const tasks = await Task.find({})
-        res.send(tasks)
+        const tasks = await Task.find({owner: req.user._id}) 
+        // await req.user.populate('tasks').execPopulate() //! this also works
+        // res.send(req.user.tasks) //* this is for this previous commented line
+        res.send(tasks) 
     } catch (e) {
-        res.status(400).send(e)
+        console.log(e);
+        res.status(500).send(e)
     }
 })
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', authMiddleware, async (req, res) => {
     const _id = req.params.id
 
     try {
-        const task = await Task.findById( _id )
+        const task = await Task.findOne({ _id, owner: req.user._id })
         if(!task) {
             return res.status(404).send('Task not found')
         }
@@ -40,7 +43,7 @@ router.get('/tasks/:id', async (req, res) => {
     }
 })
 
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', authMiddleware, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['description', 'completed']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -50,13 +53,15 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 
     try {
-        const task = await Task.findById(req.params.id)
-        updates.forEach(update => task[update] = req.body[update])
-        await task.save()
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
 
         if(!task) {
             return res.status(404).send('Task not found')
         }
+
+        updates.forEach(update => task[update] = req.body[update])
+        await task.save()
+
         res.send(task)
     } catch (e) {
         res.status(500).send(e)
@@ -65,9 +70,9 @@ router.patch('/tasks/:id', async (req, res) => {
 
 router.delete('/tasks/:id', async (req, res) => {
     try {
-        const task = await Task.findByIdAndDelete(req.params.id)
+        const task = await Task.findByOneAndDelete({ _id: req.params.id, owner: req.user._id })
         if(!task) {
-            return res.status(404).send('User not found')
+            return res.status(404).send('Task not found')
         }
         res.send(task)
     } catch(e) {

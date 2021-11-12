9ac3bff6853a84fbf383a764470a72c3465bd516
sreyom31 <sreyom31@gmail.com>
2021-11-13 01:16:16 +0530

    Wrapping up User Tests and Testing with Task Data

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,26 +1,9 @@
 const request = require('supertest')
-const jwt = require('jsonwebtoken')
-const mongoose = require('mongoose')
 const app = require('../src/app')
 const User = require('../src/models/user')
-const { use } = require('../src/routers/userRoutes')
+const { userOneId, userOne, setupDataBase } = require('./fixtures/db')
 
-const userOneId = new mongoose.Types.ObjectId()
-
-const userOne = {
-    _id: userOneId,
-    name: 'Mike',
-    email: 'mike@example.com',
-    password: '56what!!',
-    tokens: [{
-        token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
-    }]
-}
-
-beforeEach(async () => {
-  await User.deleteMany()
-  await new User(userOne).save()  
-})
+beforeEach(setupDataBase)
 
 test('Should sign up a new user', async () => {
     const response = await request(app).post('/users')
@@ -98,3 +81,34 @@ test('Should not delete account for unauthenticated user', async () => {
         .expect(401)
 })
 
+test('Should upload avatar image', async () => {
+    await request(app)
+        .post('/users/me/avatar')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .attach('avatar', 'tests/fixtures/profile-pic.jpg')
+        .expect(200)
+    const user = await User.findById(userOneId)
+    expect(user.avatar).toEqual(expect.any(Buffer))
+})
+
+test('Should update valid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            name: 'Jess'
+        })
+        .expect(200)
+    const user = await User.findById(userOneId)
+    expect(user.name).toEqual('Jess')
+})
+
+test('Should update valid user fields', async () => {
+    await request(app)
+        .patch('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            location: 'Boston'
+        })
+        .expect(400)
+})

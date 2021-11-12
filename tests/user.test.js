ffd2b79a97ca41c750b94b526c5eb0aa8bbd1334
sreyom31 <sreyom31@gmail.com>
2021-11-13 00:30:37 +0530

    Advanced assertions

--- tests/user.test.js
+++ tests/user.test.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 const app = require('../src/app')
 const User = require('../src/models/user')
+const { use } = require('../src/routers/userRoutes')
 
 const userOneId = new mongoose.Types.ObjectId()
 
@@ -22,22 +23,38 @@ beforeEach(async () => {
 })
 
 test('Should sign up a new user', async () => {
-    await request(app).post('/users')
+    const response = await request(app).post('/users')
         .send({
         name: 'Sreyom',
         email: 'sreyom@example.com',
         password: 'Mypass777!'
         })
         .expect(201)
+
+        //* Assert that the database was changed correctly
+        const user = await User.findById(response.body.user._id)
+        expect(user).not.toBeNull()
+
+        //* Assertions about the response body
+        expect(response.body).toMatchObject({
+            user: {
+                email: 'sreyom@example.com',
+                name: 'Sreyom'
+            },
+            token: user.tokens[0].token
+        })
+        expect(user.password).not.toBe('MyPass777!')
 })
 
 test('Should login existing user', async () => {
-    await request(app).post('/users/login')
+    const response = await request(app).post('/users/login')
         .send({
         email: userOne.email,
         password: userOne.password
         })
         .expect(200)
+    const user = await User.findById(userOneId)
+    expect(response.body.token).toBe(user.tokens[1].token)
 })
 
 test('Should not login non existent user', async () => {
@@ -64,15 +81,17 @@ test('Should not get profile for unauthenticated user', async () => {
         .expect(401)
 })
 
-test('Should delete profile for user', async () => {
+test('Should delete account for user', async () => {
     await request(app)
         .delete('/users/me')
         .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .send()
         .expect(200)
+    const user = await User.findById(userOneId)
+    expect(user).toBeNull()
 })
 
-test('Should not delete profile for unauthenticated user', async () => {
+test('Should not delete account for unauthenticated user', async () => {
     await request(app)
         .delete('/users/me')
         .send()

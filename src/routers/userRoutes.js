0cb063c74c4313b6f3f570aeb015d03416bbd731
sreyom31 <sreyom31@gmail.com>
2021-11-12 13:42:47 +0530

    file upload with multer and its validation

--- src/routers/userRoutes.js
+++ src/routers/userRoutes.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const User = require('../models/user')
+const multer = require('multer')
 const authMiddleware = require('../middlewares/authMiddleware')
 const router = Router()
 
@@ -77,6 +78,24 @@ router.delete('/users/me', authMiddleware, async (req, res) => {
     } catch(e) {
         res.status(500).send(e)
     }
+})
+
+const upload = multer({
+    dest: 'avatars',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload an image'))
+        }
+
+        cb(undefined, true)
+    }
+})
+
+router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+    res.send()
 })
 
 module.exports = router

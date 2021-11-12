da5085bce84d74d2510db4d0eac5987035e28c12
sreyom31 <sreyom31@gmail.com>
2021-11-12 23:36:17 +0530

    Testing an express application

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+require('./db/mongoose')
+const User = require('./models/user')
+const Task = require('./models/task')
+const userRouter = require('./routers/userRoutes')
+const taskRouter = require('./routers/taskRoutes')
+
+const app = express();
+const port = process.env.PORT
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+module.exports = app
--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-const express = require('express');
-require('./db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
-const userRouter = require('./routers/userRoutes')
-const taskRouter = require('./routers/taskRoutes')
+const app = require('./app')
 
-const app = express();
 const port = process.env.PORT
-
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
 
 app.listen(port, () => {
     console.log(`Server is running up on port ${port}`);
